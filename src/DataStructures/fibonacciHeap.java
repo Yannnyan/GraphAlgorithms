fb133d81dbf339c3dd2da8a5f59c2a790c6fe56e
Alex <regoner2@gmail.com>
2021-12-16 12:36:36 +0200

    added Consolidate() implementation

--- src/DataStructures/fibonacciHeap.java
+++ src/DataStructures/fibonacciHeap.java
@@ -10,14 +10,19 @@ import java.util.Iterator;
 public class fibonacciHeap implements api.FibonacciHeap {
     /** keyToValues is used to map Nodes to an index inside the fib heap */
     HashMap<Integer,DataTypes.Node> keyToValue;
+    /** rootKeyToValue is used to map root Nodes and get their index from the  */
+    HashMap<Integer,DataTypes.Node> rootKeyToValue;
+    ArrayList<api.Node> rootNodes;
     ArrayList<api.Node> fibHeap;
-    private int nextID=0;
+    private int nextID=0,rootNextID =0, D = 0;
     private Integer minIndex;
     /** Creates empty fibonacciHeap*/
     public fibonacciHeap(){
         minIndex = null;
         keyToValue = new HashMap<>();
         fibHeap = new ArrayList<>();
+        rootNodes = new ArrayList<>();
+
     }
     /** Initializes fibonnaciHeap from iterable collection of nodes */
     public fibonacciHeap(Iterable<DataTypes.Node> collection){
@@ -32,7 +37,23 @@ public class fibonacciHeap implements api.FibonacciHeap {
     }
     @Override
     public void consolidate() {
-
+        DataTypes.Node treeNodes[] = new DataTypes.Node[D];
+        /** for each node check the array, if its null then init the spot in the array
+         *  else, check which tree node is the minimal and attach the maximal to the minimal
+         * */
+        for (int i = 0; i < rootNodes.size(); i++) {
+            // P
+            DataTypes.Node currentNode = (DataTypes.Node) rootNodes.get(i);
+            int curDegree = currentNode.degree;
+            if(treeNodes[curDegree] == null){
+                treeNodes[curDegree] = currentNode;
+            }
+            else
+            {
+                treeNodes[curDegree] = treeNodes[curDegree].key < currentNode.key ?
+                        treeNodes[curDegree] : currentNode;
+            }
+        }
     }
 
     @Override
@@ -41,7 +62,10 @@ public class fibonacciHeap implements api.FibonacciHeap {
         if(minIndex == null)    minIndex = Integer.valueOf(0);
         else if(minIndex > node.key)     minIndex = node.key;
         keyToValue.put(Integer.valueOf(nextID++), node);
+        rootKeyToValue.put(Integer.valueOf(rootNextID), node);
         fibHeap.add(node);
+        rootNodes.add(node);
+
     }
 
     @Override
@@ -49,6 +73,7 @@ public class fibonacciHeap implements api.FibonacciHeap {
         if(fibHeap.size() == 1){
             minIndex = null;
         }
+        DataTypes.Node minNode = rootKeyToValue.get(minIndex);
 
         return null;
     }
@@ -56,5 +81,10 @@ public class fibonacciHeap implements api.FibonacciHeap {
     @Override
     public void decreaseKey(DataTypes.Node num) {
 
+    }
+
+    @Override
+    public String toString() {
+        return "fibonacciHeap{}";
     }
 }
---
+++ src/Tests/DataStructures/fibonacciHeapTest.java
@@ -0,0 +1,36 @@
+package DataStructures;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class fibonacciHeapTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        // print the fib heap
+    }
+
+    @org.junit.jupiter.api.Test
+    void consolidate() {
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteMin() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decreaseKey() {
+    }
+}
